a670ce91e8a9c5c31062b103d22a798916705b3b
Alejandro Ríos Jasso <jassoalex97@hotmail.com>
2019-02-25 22:59:23 -0600

    Update Hanoi Recursive 6125 IC and comments

--- ASM/tower of hanoi.asm
+++ ASM/tower of hanoi.asm
@@ -1,26 +1,26 @@
-# Author: Alejandro Rios
-# Exp: is708932
+# Authors: Alejandro Rios, Javier Ochoa
+# Exp: is708932, 
 # Date: Feb 25, 2019
 # Tower Of Hanoi
 
 .text
-	addi $s0, $zero, 8
+	addi $s0, $zero, 8 # Number of disks
 	addi $s1, $s1, 0x1001
-	sll $s1, $s1, 16
-	addi $s2, $s1, 0x20
-	addi $s3, $s2, 0x20
+	sll $s1, $s1, 16 # Save address of tower A
+	addi $s2, $s1, 0x20 # Save address of tower B
+	addi $s3, $s2, 0x20 # Save address of tower C
 	add $t0, $s0, $zero
 	
-	for:
+	for: # Fill tower A with all the disks
 		beq $t0, $zero, end_for
 		sw $t0, 0($s1)
 		addi $s1, $s1, 4
 		addi $t0, $t0, -1
 		j for
 		end_for:
-		
+	
 	main:
-		add $a0, $s0, $zero
+		add $a0, $s0, $zero # Biggest disk
 		add $a1, $s1, $zero # Source
 		add $a2, $s3, $zero # Destination
 		add $a3, $s2, $zero # Spare
@@ -31,9 +31,10 @@
 		addi $sp, $sp, -8 # Save space in stack
 		sw $ra, 0($sp) # Save return address
 		sw $a0, 4($sp) # Save disk
-		beq $a0, 1, equal_one # if (disk == 1)
+		# If (disk == 1)
+		beq $a0, 1, equal_one 
 		# Else
-		addi $a0, $a0, -1 # Look for a smaller disks (first)
+		addi $a0, $a0, -1 # Look for a smaller disk (first)
 		add $t0, $a2, $zero # temp = dest
 		add $a2, $a3, $zero # dest = spare
 		add $a3, $t0, $zero # spare = temp
@@ -41,10 +42,9 @@
 		add $t0, $a2, $zero # temp = dest
 		add $a2, $a3, $zero # dest = spare
 		add $a3, $t0, $zero # spare = temp
-		lw $a0, 4($sp) # Get the disk for the general call
 		jal move_disk # Move disk from source to dest
 		lw $a0, 4($sp) # Get the disk for the general call
-		addi $a0, $a0, -1 # Look for a smaller disks (second)
+		addi $a0, $a0, -1 # Look for a smaller disk (second)
 		add $t0, $a1, $zero # temp = source
 		add $a1, $a3, $zero # source = spare
 		add $a3, $t0, $zero # spare = temp
@@ -54,24 +54,23 @@
 		add $a3, $t0, $zero # spare = temp
 		j end_if # End of if statement
 		 
-	equal_one: # if (disk == 1)
+	equal_one:
 		jal move_disk # Move disk from source to dest (base case)
 		
 	end_if: # Save stack values
-		lw $ra, 0($sp) # Return address
-		lw $a0, 4($sp) # Disk
+		lw $ra, ($sp) # Return address
 		addi $sp, $sp, 8 # Bring back address in stack pointer
 		jr $ra # Return to recursive general
 		
 	move_disk:
 	# pop - push in line *********************************************
-	# pop
+		# pop
 		addi $a1, $a1, -4
 		lw $t0, ($a1) # Get value of element at the top
-		sw $zero, ($a1)
-	# push
-		sw $t0, ($a2)
-		addi $a2, $a2, 4
+		sw $zero, ($a1) # Write 0 in white space
+		# push
+		sw $t0, ($a2) # Push at the top of the tower
+		addi $a2, $a2, 4 # Update top pointer
 		jr $ra # Return from move_disk
 		
 	exit: # End
